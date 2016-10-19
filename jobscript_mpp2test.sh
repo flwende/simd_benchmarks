8d37760bba252b4de7440ec9fcf013a8fde421eb
noma <ma.noack.pr@gmail.com>
2016-10-19 17:19:35 +0200

    Added a jobscript for the test queue of the Haswell partition of the HLRN.

---
+++ jobscript_mpp2test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#PBS -A # account for cpu time
+#PBS -N simd_benchmarks_hsw # job name
+
+#PBS -q mpp2testq
+#PBS -l nodes=1:ppn=24 
+#PBS -l walltime=01:00:00
+
+#xxPBS -o results/$JOBNAME.o$JOBID
+#PBS -j oe # join error into output
+
+
+# cray compiler
+module switch cce/8.5.3
+
+# intel compiler
+module load intel/17.0.0.098
+
+# gcc
+export PATH=$HOME/Software/gcc-6.2.0/bin:$PATH
+export LD_LIBRARY_PATH=$HOME/Software/gcc-6.2.0/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$HOME/Software/gcc-6.2.0/lib/:$LD_LIBRARY_PATH
+
+# clang
+export PATH=$HOME/Software/llvm_3.9.0/bin:$PATH
+export LD_LIBRARY_PATH=$HOME/Software/llvm_3.9.0/lib:$LD_LIBRARY_PATH
+
+module switch craype-ivybridge craype-haswell
+
+export INTEL_LICENSE_FILE=27000@sdb
+
+aprun -n 1 -N 1 -cc none ./benchmark.cray.sh
+aprun -n 1 -N 1 -cc none ./benchmark.intel.sh
+aprun -n 1 -N 1 -cc none ./benchmark.gnu.sh
+aprun -n 1 -N 1 -cc none ./benchmark.clang.sh
+
