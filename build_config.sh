b0383c03456bd173176b8114af7e191062b6a26b
Matthias Noack <ma.noack.pr@gmail.com>
2018-03-07 00:51:36 +0100

    Switched to UMESIMD

--- build_config.sh
+++ build_config.sh
@@ -4,32 +4,32 @@
 
 function build_config_hsw {
 	export VC_ROOT=${HOME}/Software/Vc
-	export VC_AVAILABLE=yes
-#	export VC_AVAILABLE=no
+#	export VC_AVAILABLE=yes
+	export VC_AVAILABLE=no
 
 	export VCL_ROOT=${HOME}/Software/vcl
 #	export VCL_AVAILABLE=yes
 	export VCL_AVAILABLE=no
 
 	export UMESIMD_ROOT=${HOME}/Software/umesimd
-#	export UMESIMD_AVAILABLE=yes
-	export UMESIMD_AVAILABLE=no
+	export UMESIMD_AVAILABLE=yes
+#	export UMESIMD_AVAILABLE=no
 
 	export PLATFORM=x86
 }
 
 function build_config_skl {
 	export VC_ROOT=${HOME}/Software/Vc
-	export VC_AVAILABLE=yes
-#	export VC_AVAILABLE=no
+#	export VC_AVAILABLE=yes
+	export VC_AVAILABLE=no
 
 	export VCL_ROOT=${HOME}/Software/vcl
 #	export VCL_AVAILABLE=yes
 	export VCL_AVAILABLE=no
 
 	export UMESIMD_ROOT=${HOME}/Software/umesimd
-#	export UMESIMD_AVAILABLE=yes
-	export UMESIMD_AVAILABLE=no
+	export UMESIMD_AVAILABLE=yes
+#	export UMESIMD_AVAILABLE=no
 
 	export PLATFORM=x86
 }
@@ -44,8 +44,8 @@ function build_config_knl {
 	export VCL_AVAILABLE=no
 
 	export UMESIMD_ROOT=${HOME}/Software/umesimd
-#	export UMESIMD_AVAILABLE=yes
-	export UMESIMD_AVAILABLE=no
+	export UMESIMD_AVAILABLE=yes
+#	export UMESIMD_AVAILABLE=no
 
 	export PLATFORM=x86
 }
